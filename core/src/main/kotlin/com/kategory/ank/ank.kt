70a591ebf35ffa0c53081d219fe7d5cb355b6297
Raul Raja <raulraja@gmail.com>
2017-08-17 13:11:59 +0200

    bring branch to compile state

--- core/src/main/kotlin/com/kategory/ank/ank.kt
+++ core/src/main/kotlin/com/kategory/ank/ank.kt
@@ -6,17 +6,20 @@ import java.io.File
 
 const val AnkBlock = "kotlin:ank"
 
-fun ank(source: File, target: File, compilerArgs: ListKW<String>): HK<FreeHK<AnkOpsHK>, ListKW<File>> =
+fun ank(source: File, target: File, compilerArgs: ListKW<String>) =
         AnkOps.binding {
             val targetDirectory: File = createTarget(source, target).bind()
             val files: ListKW<File> = getFileCandidates(targetDirectory).bind()
-            val filesContents: ListKW<String> = files.map(::readFile).sequence().bind()
-            val parsedMarkDowns: ListKW<ASTNode> = filesContents.map(::parseMarkdown).sequence().bind()
+            val filesContents: ListKW<String> = files.map(::readFile).k().sequence().bind()
+            val parsedMarkDowns: ListKW<ASTNode> = filesContents.map(::parseMarkdown).k().sequence().bind()
             val sources: ListKW<String> = ListKW(parsedMarkDowns.mapIndexed { n, tree ->
                 extractCode(filesContents.list[n], tree)
-            }).sequence().bind()
-            ListKW(sources.mapIndexed { n, s -> compileCode(files.list[n], s, compilerArgs) }).sequence().bind()
-            val replacedResults: ListKW<String> = filesContents.map(::replaceAnkToKotlin).sequence().bind()
+            }).k().sequence().bind()
+            ListKW(sources.mapIndexed { n, s -> compileCode(files.list[n], s, compilerArgs) }).k().sequence().bind()
+            val replacedResults: ListKW<String> = filesContents.map(::replaceAnkToKotlin).k().sequence().bind()
             val resultingFiles: ListKW<File> = generateFiles(files, replacedResults).bind()
             yields(resultingFiles)
         }
+
+fun <A> ListKW<Free<AnkOpsHK, A>>.sequence(T: Traverse<ListKWHK> = traverse()): Free<AnkOpsHK, ListKW<A>> =
+        T.sequence(this, AnkOps).ev().map { it.ev() }
