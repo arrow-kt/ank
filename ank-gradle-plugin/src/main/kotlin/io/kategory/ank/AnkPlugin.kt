0a554fd09643916f0dab6456c3dd55aee2b7a772
Guardiola31337 <pablo.guardiola@mapbox.com>
2017-08-21 10:37:12 +0200

    translate ank-gradle-plugin into kotlin

---
+++ ank-gradle-plugin/src/main/kotlin/io/kategory/ank/AnkPlugin.kt
@@ -0,0 +1,37 @@
+package io.kategory.ank
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.DependencyResolutionListener
+import org.gradle.api.artifacts.ResolvableDependencies
+import org.gradle.api.tasks.JavaExec
+
+
+class AnkPlugin : Plugin<Project> {
+
+    companion object {
+        private val EXTENSION_NAME = "ank"
+        private val TASK_NAME = "runAnk"
+        private val ANK_CORE_DEPENDENCY = "io.kategory:ank-core:0.1.0"
+    }
+
+    override fun apply(target: Project) {
+        val compileDeps = target.configurations.getByName("compile").dependencies
+        target.gradle.addListener(object : DependencyResolutionListener {
+            override fun beforeResolve(resolvableDependencies: ResolvableDependencies) {
+                compileDeps.add(target.dependencies.create(ANK_CORE_DEPENDENCY))
+                target.gradle.removeListener(this)
+            }
+
+            override fun afterResolve(resolvableDependencies: ResolvableDependencies) {}
+        })
+        val extension = AnkExtension()
+        target.extensions.add(EXTENSION_NAME, extension)
+        target.afterEvaluate { project ->
+            val task = target.tasks.create(TASK_NAME, JavaExec::class.java)
+            task.classpath = extension.classpath
+            task.main = "io.kategory.ank.main"
+            task.setArgs(extension.arguments)
+        }
+    }
+}
