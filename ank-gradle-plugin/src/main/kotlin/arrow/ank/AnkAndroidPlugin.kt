18454c946f3285fed628492fdc8c28c1b1c49c37
Pablo Gonzalez Alonso <pabs87@gmail.com>
2018-06-18 14:28:23 +0100

    [WIP] broken build

--- ank-gradle-plugin/src/main/kotlin/arrow/ank/AnkAndroidPlugin.kt
+++ ank-gradle-plugin/src/main/kotlin/arrow/ank/AnkAndroidPlugin.kt
@@ -1,32 +1,53 @@
 package arrow.ank
 
 import com.android.build.gradle.LibraryPlugin
+import com.android.build.gradle.internal.scope.VariantScope
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
 import org.gradle.api.tasks.JavaExec
+import java.io.File
 
 class AnkAndroidPlugin : Plugin<Project> {
 
-    companion object {
-        private const val EXTENSION_NAME = "ank"
-        private const val TASK_NAME = "runAnk"
-    }
-
-    override fun apply(target: Project) {
+    override fun apply(target: Project) = with(target) {
         val extension = AnkExtension()
-        target.extensions.add(EXTENSION_NAME, extension)
-        target.afterEvaluate {
-            target.plugins.findPlugin(LibraryPlugin::class.java)?.variantManager?.variantDataList?.forEach({ variant ->
-                target.tasks.create("${variant.name}$TASK_NAME", JavaExec::class.java).apply {
-
-//                    classpath = target.files(variant.variantConfiguration)
-                    main = "arrow.ank.main"
-                }.args = mutableListOf(
-                        extension.source!!.absolutePath,
-                        extension.target!!.absolutePath + "/" + variant.name,
-                        ""//*variant.variantConfiguration.compileClasspath.map { it.toURI().toURL().toString() }.toTypedArray()
-                )
-            })
+        extensions.add("ank", extension)
+        afterEvaluate {
+            plugins.findPlugin(LibraryPlugin::class.java)?.variantScopes
+                ?.forEach { scope -> createAnkTask(scope, extension) }
+                ?: error("Android library plugin not found")
         }
     }
+
+    private fun Project.createAnkTask(scope: VariantScope, extension: AnkExtension) {
+        task<JavaExec>(scope.variantData.getTaskName("runAnk", "")) {
+            group = "ank"
+            classpath = files(scope.classpath)
+            main = "arrow.ank.main"
+            args = ankArguments(
+                source = scope.javaOutputDir, //extension.source ?: File("."),
+                target = File(extension.target ?: File("."), "/${scope.fullVariantName}"),
+                classpath = scope.classpath
+            )
+        }
+    }
+
 }
+
+private val LibraryPlugin.variantScopes
+    get() = variantManager.variantScopes
+
+private inline fun <reified T : Task> Project.task(name: String, crossinline block: T.() -> Unit) =
+    tasks.create(name, T::class.java) { it.block() }
+
+private fun ankArguments(source: File, target: File, classpath: Set<File>): List<String> =
+    listOf(source.absolutePath, target.absolutePath, *classpath.toPathArray())
+
+private fun Set<File>.toPathArray(): Array<String> =
+    map { it.toURI().toURL().toString() }.toTypedArray()
+
+private val VariantScope.classpath
+    get() =
+        variantData.variantDependency.compileClasspath.resolve()
+
